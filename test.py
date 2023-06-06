55c187d0acf2ef3cfa53d2e2bcef6b5808289be4
DemoWorkshop <89449194+DemoWorkshop@users.noreply.github.com>
2023-06-06 11:25:43 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
+import subprocess
+
+subprocess.run("git pull origin", creationflags=subprocess.CREATE_NO_WINDOW)
+
 l=[]
 for i in range(69):
   print(i)
   l.append(i)
 print(l)
 print("bye bye")
-
-import subprocess
-subprocess.run("git pull origin", creationflags=subprocess.CREATE_NO_WINDOW)
