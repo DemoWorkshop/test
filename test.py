3fc407542663553c3c801cab20e5c755585bc480
DemoWorkshop <89449194+DemoWorkshop@users.noreply.github.com>
2023-06-06 11:27:57 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import subprocess
 import os
 
-subprocess.run("git pull origin", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
-
 l=[]
 for i in range(69):
   print(i)
   l.append(i)
 print(l)
 print("bye bye")
+
+
+subprocess.run("git pull origin", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
